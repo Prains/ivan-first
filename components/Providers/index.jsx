9d9cacd5acdd9aaa70e9e69c6ae8eac2985f0d00
Prains <sleskov2013@yandex.ru>
2023-05-19 14:40:14 +0300

    add providers to layout

--- components/Providers/index.jsx
+++ components/Providers/index.jsx
@@ -1,7 +1,14 @@
+"use client";
 import { Provider } from "react-redux";
+import { ChakraProvider } from "@chakra-ui/react";
+import store from "@/services/store";
 
 const Providers = ({ children }) => {
-  return <Provider>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <ChakraProvider>{children}</ChakraProvider>
+    </Provider>
+  );
 };
 
 export default Providers;
