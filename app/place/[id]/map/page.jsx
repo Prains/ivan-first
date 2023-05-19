77cfa9b3d080f5949cad702699fd69343fb0bf53
Prains <sleskov2013@yandex.ru>
2023-05-19 21:31:34 +0300

    fix map size

--- app/place/[id]/map/page.jsx
+++ app/place/[id]/map/page.jsx
@@ -10,7 +10,7 @@ const PlaceMap = () => {
           <iframe
             src="https://yandex.ru/map-widget/v1/?um=constructor%3Ae0b0022645a6c40fee5c51227945eb7b699e63822f7a54c42730b1f07e2b47a3&amp;source=constructor"
             width="100%"
-            height="400"
+            height="450px"
             frameborder="0"
           ></iframe>
         </div>
