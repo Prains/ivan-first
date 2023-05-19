99e205ffae149b86069d23dfe1df56f38954420d
Prains <sleskov2013@yandex.ru>
2023-05-19 15:28:39 +0300

    styling

--- app/place/[id]/page.jsx
+++ app/place/[id]/page.jsx
@@ -6,13 +6,15 @@ const Place = () => {
   return (
     <main>
       <section className="bg-[#EBF8FF] rounded-xl flex-center-col">
-        <Image src={bgPlace} alt="Artplay" />
+        <Image src={bgPlace} alt="Artplay" className="w-full" />
         <div className="w-[91%] my-0 mx-auto">
           <article>
-            <h4 className="text-[#000000] text-xl">Artplay</h4>
-            <p>
+            <h4 className="text-[#000000] text-xl mb-2">Artplay</h4>
+            <p className="flex-center-col items-start gap-1 mb-3 text-[#535353]">
               ул. Нижняя Сыромятническая, д. 10
-              <Link href="/">Показать на карте</Link>
+              <Link href="/" className="underline text-[#4F4F4F]">
+                Показать на карте
+              </Link>
             </p>
           </article>
         </div>
