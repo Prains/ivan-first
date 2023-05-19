6618cc5b00fba6f3ede1bcdfb504abe7f08c4bee
Prains <sleskov2013@yandex.ru>
2023-05-19 18:00:07 +0300

    change button price height

--- components/Place/PlacePriceNOrder/PlacePriceNOrder.jsx
+++ components/Place/PlacePriceNOrder/PlacePriceNOrder.jsx
@@ -5,7 +5,7 @@ const PlacePriceNOrder = () => {
   return (
     <>
       <p className="text-[18px] mb-3 text-[#000000] font-medium">18000 р/час</p>
-      <Button className="mb-3 mx-auto w-full" colorScheme="red" size="sm">
+      <Button className="mb-3 mx-auto w-full h-[40px]" colorScheme="red">
         Забронировать
       </Button>
     </>
