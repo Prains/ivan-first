156ed8dc3f938192921243567b5ab6c19aba8783
Prains <sleskov2013@yandex.ru>
2023-05-19 17:42:33 +0300

    add link from place to map

--- components/Place/PlaceCommonInfo/PlaceCommonInfo.jsx
+++ components/Place/PlaceCommonInfo/PlaceCommonInfo.jsx
@@ -6,7 +6,7 @@ const PlaceCommonInfo = () => {
       <h4 className="text-[#000000] text-xl mb-2">Artplay</h4>
       <p className="flex-center-col items-start gap-1 mb-3 text-[#535353]">
         ул. Нижняя Сыромятническая, д. 10
-        <Link href="/" className="underline text-[#4F4F4F]">
+        <Link href="/place/1/map" className="underline text-[#4F4F4F]">
           Показать на карте
         </Link>
       </p>
