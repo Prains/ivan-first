79696c40ba7b52151806cd6401193b851d9abc3d
Prains <sleskov2013@yandex.ru>
2023-05-19 17:21:02 +0300

    add arrowback ui component

--- images/icons/index.js
+++ images/icons/index.js
@@ -1,6 +1,7 @@
 import userIcon from "./Group 21.svg";
 import logo from "./Group 38.svg";
-import vk from './vk.cac498d8.svg.svg'
-import tg from './tg.1d36582b.svg.svg'
+import vk from "./vk.cac498d8.svg.svg";
+import tg from "./tg.1d36582b.svg.svg";
+import whiteArrowLeft from "./FiArrowLeft.svg";
 
-export { userIcon, logo, vk, tg };
+export { userIcon, logo, vk, tg, whiteArrowLeft };
