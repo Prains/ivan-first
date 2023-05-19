448cc7353ee9ab65c2e2aa04b36ae902fd1f1956
Prains <sleskov2013@yandex.ru>
2023-05-19 17:43:10 +0300

    add footer to layout

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import Providers from "@/components/Providers";
 import "./globals.css";
+import Footer from "@/components/Footer/Footer";
 
 export const metadata = {
   title: "Create Next App",
@@ -13,6 +14,7 @@ export default function RootLayout({ children, popups }) {
         <Providers>
           {children}
           {popups}
+          <Footer/>
         </Providers>
       </body>
     </html>
