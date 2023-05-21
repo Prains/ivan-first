f39475254b87a8287f4d4011ad151c2035c4a689
Pazific <advolatum@yandex.ru>
2023-05-21 23:54:43 +0300

    refactor: delete className prop in AuthLink

--- components/Auth/AuthLink/AuthLink.jsx
+++ components/Auth/AuthLink/AuthLink.jsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-const AuthLink = ({ path, className, children }) => {
+const AuthLink = ({ path, children }) => {
     const pathname = usePathname();
 
     return (
