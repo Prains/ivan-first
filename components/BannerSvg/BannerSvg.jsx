6704d96ab8d35ab38477366506d4e90f5148a751
Roman Maximov <100602249+realchayka@users.noreply.github.com>
2023-05-23 15:14:41 +0300

    fixed banner

--- components/BannerSvg/BannerSvg.jsx
+++ components/BannerSvg/BannerSvg.jsx
@@ -1,11 +1,10 @@
-import styles from './BannerSvg.module.css'
 import Image from "next/image";
 import {bannerVector} from "@/images/Banner";
 
-const BannerSvg = () => {
+const BannerSvg = ({className}) => {
     return (
         <div>
-            <Image className={styles.bannerSvg} src={bannerVector} alt='hello' />
+            <Image className={className} src={bannerVector} alt='hello' />
         </div>
     );
 };
