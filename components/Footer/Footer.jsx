3dbe56307103d056f171dd1edaa8d033dc14f181
Prains <sleskov2013@yandex.ru>
2023-05-24 00:07:24 +0300

    add footer desktop

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,9 +1,11 @@
+import FooterDesktop from "./FooterDesktop/FooterDesktop";
 import FooterMobile from "./FooterMobile/FooterMobile";
 
 const Footer = () => {
   return (
-    <footer className="bg-black z-0 relative">
+    <footer className="bg-black z-0 relative max-w-[1280px] my-0 mx-auto">
       <FooterMobile />
+      <FooterDesktop />
     </footer>
   );
 };
--- components/Footer/FooterLink/FooterLink.jsx
+++ components/Footer/FooterLink/FooterLink.jsx
@@ -2,7 +2,10 @@ import Link from "next/link";
 
 const FooterLink = ({ children, href }) => {
   return (
-    <Link href={href} className="h-[27px] text-white text-sm">
+    <Link
+      href={href}
+      className="h-[27px] text-white text-sm lg:text-base lg:h-[29px]"
+    >
       {children}
     </Link>
   );
