1575f6e885694229c347e6cac44bc6238b5a72a3
Prains <sleskov2013@yandex.ru>
2023-05-24 21:28:47 +0300

    123

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import FooterMobile from "./FooterMobile/FooterMobile";
 
 const Footer = () => {
   return (
-    <footer className="bg-black z-0 relative max-w-[1280px] my-0 mx-auto">
+    <footer className="bg-[#0C1622] z-0 relative max-w-[1280px] my-0 mx-auto">
       <FooterMobile />
       <FooterDesktop />
     </footer>
