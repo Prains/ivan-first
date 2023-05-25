fa904b3f9404dd2de360a4fea59cef6e0049ed8b
Prains <sleskov2013@yandex.ru>
2023-05-25 19:58:58 +0300

    123

--- components/Profile/ProfileNewPlace/ProfileNewPlace.jsx
+++ components/Profile/ProfileNewPlace/ProfileNewPlace.jsx
@@ -7,11 +7,15 @@ const ProfileNewPlace = () => {
       <section>
         <h1>Публикация новой креактивной площадки</h1>
         <div>
+          <IsolatedInput placeholder="Название новой площадки" />
+          <IsolatedInput placeholder="Адрес площадки" />
           <IsolatedInput />
-          <IsolatedInput />
-          <IsolatedInput />
-          <IsolatedTextArea />
+          <IsolatedTextArea placeholder="Описание креативной площадки" />
         </div>
+      </section>
+      <section>
+        <h2>Время работы площадки:</h2>
+        <div></div>
       </section>
     </main>
   );
