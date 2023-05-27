d62c413e5570d2b97fa2e7403699607d8a03e83e
Pazific <advolatum@yandex.ru>
2023-05-28 02:53:45 +0300

    bugfixes

--- components/Profile/ArchiveBooking/ArchiveBookingMobile/ArchiveBookingMobile.jsx
+++ components/Profile/ArchiveBooking/ArchiveBookingMobile/ArchiveBookingMobile.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import ArchiveBookingMobileItem from "@/components/Profile/ArchiveBooking/ArchiveBookingMobileItem/ArchiveBookingMobileItem";
 import IsolatedAccordeonLandLord from "@/components/ui/IsolatedAccordeonLandLord/IsolatedAccordeonLandLord";
 import useFindUser from "@/hooks/useFindUser";
@@ -6,7 +6,6 @@ import Link from "next/link";
 import links from "@/utils/links";
 import { useEffect, useState } from "react";
 import api from "@/utils/api";
-import ArchiveBookingMobileList from "../ArchiveBookingMobileList/ArchiveBookingMobileList";
 
 const ArchiveBookingMobile = () => {
     const user = useFindUser();
@@ -27,30 +26,41 @@ const ArchiveBookingMobile = () => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
     return (
-        <section className='block lg:hidden bg-[#0C1622] min-h-[98vh] bg-bgAuth-mobile bg-top bg-no-repeat bg-cover relative px-[16px] py-[16px] w-full'>
-            {
-                user.userRole === "landlord" ?
-                    <>
-                        <IsolatedAccordeonLandLord isAccept={true} isActive={false} isPending={false} data={data}
-                                                   titleAccordeon={'Artplay'}/>
-                        <IsolatedAccordeonLandLord isAccept={false} isActive={true} isPending={true} data={data}
-                                                   titleAccordeon={'Artplay(2)'}/>
-                    </> :
-                    <>
-                        <div className='py-[14px] px-[12px] bg-[#EBF8FF] mt-[18px] rounded-[6px] pb-[60px]'>
-                            {books.map((book) => {
-                                return (
-                                    book.book.status === "resolved" && <ArchiveBookingMobileItem key={book.id} {...book} userRole={user.userRole} />
+        <section className="block lg:hidden bg-[#0C1622] min-h-[98vh] bg-bgAuth-mobile bg-top bg-no-repeat bg-cover relative px-[16px] py-[16px] w-full">
+            {user.userRole === "landlord" ? (
+                <>
+                    <IsolatedAccordeonLandLord
+                        isAccept={true}
+                        isActive={false}
+                        isPending={false}
+                        data={data}
+                        titleAccordeon={"Artplay"}
+                    />
+                    <IsolatedAccordeonLandLord
+                        isAccept={false}
+                        isActive={true}
+                        isPending={true}
+                        data={data}
+                        titleAccordeon={"Artplay(2)"}
+                    />
+                </>
+            ) : (
+                <>
+                    <div className="py-[14px] px-[12px] bg-[#EBF8FF] mt-[18px] rounded-[6px] pb-[60px]">
+                        {books.map((book) => {
+                            return (
+                                book.book.status === "resolved" && (
+                                    <ArchiveBookingMobileItem
+                                        key={book.id}
+                                        {...book}
+                                        userRole={user.userRole}
+                                    />
                                 )
-                            })
-                            
-                            }
-                            {/* {
-                                data.map(item =>   <ArchiveBookingMobileItem key={item.id} {...item}/>)
-                            } */}
-                        </div>
-                    </>
-            }
+                            );
+                        })}
+                    </div>
+                </>
+            )}
         </section>
     );
 };
